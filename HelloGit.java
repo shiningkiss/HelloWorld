ef04293a9fafb028dd2f2a94d0f37ab9404b2b0f
issuser <issuser@ISS110301001047.isoftstone.com>
2018-11-19 10:57:07 +0800

    test commit

---
+++ HelloGit.java
@@ -0,0 +1,11 @@
+/*
+* @Author: issuser
+* @Date:   2018-11-19 10:53:32
+* @Last Modified by:   issuser
+* @Last Modified time: 2018-11-19 10:54:58
+*/
+public class HelloGit{
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
